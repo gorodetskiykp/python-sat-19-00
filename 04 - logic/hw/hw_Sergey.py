dedc812d0067d322cbf1c22099a9f30b66e6b3da
sergej <sfomenko12@yandex.ru>
2023-07-27 23:08:02 +0300

    Выполнил дз "21"

--- 04 - logic/hw/hw_Sergey.py
+++ 04 - logic/hw/hw_Sergey.py
@@ -2,7 +2,7 @@ from random import randint
 
 
 counter_card = 2
-check_card = randint(2,11) + randint(2,11)
+check_card = randint(2, 11) + randint(2, 11)
 print("Счет:", check_card)
 
 
@@ -14,7 +14,7 @@ else:
         choice = input("Берем карту или нет? Ваш выбор:").upper()
         if choice == "ДА":
             counter_card += 1
-            new_card = randint(2,11)
+            new_card = randint(2, 11)
             if new_card == 11:
                 new_card = 1
                 check_card += new_card
