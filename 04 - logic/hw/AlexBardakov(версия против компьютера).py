cbbdcfb6493e5652061e742770d401ffb706bd7c
KompukterMl <Battleover@yandex.ru>
2023-07-29 03:49:49 +0700

    Исправил указанные замечания, а в частности: избавился от global загнал рандом под функцию избавился от принтов в функциях
    В том числе дополнил код условиями замены 11 на 1 в ситуации, когда 11 уже на руке или когда берем новую 11.

--- 04 - logic/hw/AlexBardakov(версия против компьютера).py
+++ 04 - logic/hw/AlexBardakov(версия против компьютера).py
@@ -1,57 +1,63 @@
+import sys
 from random import randint
 
-hand = []  # Запоминаем карты с руки, чтобы выводить потом ч/з print
-card1 = randint(2, 11)
-card2 = randint(2, 11)
 
-comp_hand = []  # У компьютера теперь тоже есть ручки
-comp_card1 = randint(2, 11)
-comp_card2 = randint(2, 11)
+def rand():  # Исправил повторы, загнал под функцию
+    a = randint(2, 11)
+    if a == 11 and sum(hand):
+        a = 1
+    return a
 
 
 def yes():
     new_card = randint(2, 11)
     hand.append(new_card)
-    print("У вас на руках карты:", hand,
-          "Ваша сумма карт:", sum(hand), "Нужна ли еще карта? да/нет")
-    global choice
-    choice = input("Сделайте свой выбор:")
 
 
-def lose():
-    print("ВЫ ПРОИГРАЛИ!")
-    return
+hand = []  # Запоминаем карты с руки, чтобы выводить потом ч/з print
+card1 = rand()
+card2 = rand()
 
+comp_hand = []  # У компьютера теперь тоже есть ручки
+comp_card1 = rand()
+comp_card2 = rand()
 
 hand.append(card1)  # Берем в руку первые карты
 hand.append(card2)
 comp_hand.append(comp_card1)  # Компьютеру тоже даем карты, все честно
 comp_hand.append(comp_card2)
 
-# Сначала посмотрим, что у компьютера и разберемся со своей рукой
+# Мы видим свою руку и одну карту крупье
 print("У вас на руках карты:", hand, "Ваша сумма карт:", sum(hand),
-      "\nУ компьютера:", comp_hand, "\nНужна ли еще карта? да/нет")
-choice = input("Сделайте свой выбор:")
+      "\nУ компьютера:", comp_hand[0], "\nНужна ли еще карта? да/нет")
 
-while choice == "да":
-    yes()
-
-if choice == "нет":
-    print("У вас на руках карты:", hand,
-          "\nВаша сумма карт:", sum(hand))
-
-# Условие моментального проигрыша
-if sum(hand) > 21:
-    lose()
+for i in range(10):  # Использовал цикл for, чтобы убрать global
+    choice = input("Сделайте свой выбор:")
+    if choice in ["да", "Да", "ДА"]:
+        yes()
+        if sum(hand) > 21:
+            if 11 in hand:  # Условие замены 11 на 1
+                hand.remove(11)
+                hand.append(1)
+                print("У вас на руках карты:", hand,
+                      "Ваша сумма карт:", sum(hand), "Нужна ли еще карта? да/нет")
+            else:  # Условие моментального проигрыша
+                print("Ваша сумма карт:", sum(hand),"\nВЫ ПРОИГРАЛИ!")
+                sys.exit()
+        else:
+            print("У вас на руках карты:", hand,
+                  "Ваша сумма карт:", sum(hand), "Нужна ли еще карта? да/нет")
+    elif choice in ["нет", "Нет", "НЕТ"]:
+        print("У вас на руках карты:", hand, "Ваша сумма карт:", sum(hand))
+        break
 
 # Компьютер производит добор карт. Да, он жульничает.
 while sum(comp_hand) < 17 and sum(hand) > sum(comp_hand):
-    new_card = randint(2, 11)
+    new_card = rand()
     comp_hand.append(new_card)
-    print("У компьютера:", comp_hand, "\nВсего:", sum(comp_hand))
 
-print("Считаем результат! Сумма ваших карт:", sum(hand),
-      "\nУ компьютера:", sum(comp_hand))
+# Компьютер вскрывается
+print("У компьютера:", comp_hand, "Всего:", sum(comp_hand))
 
 if sum(hand) <= 21 and (sum(comp_hand) > 21 or sum(comp_hand) < sum(hand)) :
     print("ВЫ ВЫИГРАЛИ")
