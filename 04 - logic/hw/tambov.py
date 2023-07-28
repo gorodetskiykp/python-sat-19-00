060d656fc2e29e67e24cd08a50093cb9d85d134a
tmbkv <costa09@yandex.ru>
2023-07-28 16:46:45 +0300

    ДЗ № 4, доработал код

--- 04 - logic/hw/tambov.py
+++ 04 - logic/hw/tambov.py
@@ -6,30 +6,29 @@ number.append(randint(2, 11))
 number.append(randint(2, 11))
 print(number)
 
-if sum(number) == 21:
+choice = input("Сделайте свой выбор, y/n: ")
+if choice == 'y':
+    number_3 = randint(2, 11)
+    print(number_3)
+    number.append(number_3)
+    if 11 in number:
+        number.remove(11)
+        number.append(1)
+        if sum(number) == 21:
+            print(sum(number), 'Произошла замена 11, вы выиграли!')
+        elif sum(number) < 21:
+            print(sum(number), 'Произошла замена 11, вы не проиграли')
+    elif sum(number) == 21:
+        print(sum(number), 'Вы выиграли!')
+    elif sum(number) < 21:
+        print(sum(number), 'Вы не проиграли')
+    elif sum(number) > 21:
+        print(sum(number), 'Вы проиграли')
+elif sum(number) == 21:
     print(sum(number), 'Вы выиграли!')
 elif sum(number) == 22:
     print(sum(number), 'Вы проиграли')
-elif sum(number) < 21:
-    choice = input("Сделайте свой выбор, y/n: ")
-    if choice == 'y':
-        number_3 = randint(2, 11)
-        print(number_3)
-        number.append(number_3)
-        if sum(number) == 21:
-            print(sum(number), 'Вы выиграли!')
-        elif 11 in number:
-            number.remove(11)
-            number.append(1)
-            if sum(number) == 21:
-                print(sum(number), 'Произошла замена 11, вы выиграли!')
-            else:
-                print(sum(number), 'Произошла замена 11, вы не проиграли')
-        elif sum(number) < 21:
-            print(sum(number), 'Вы не проиграли')
-        else:
-            print(sum(number), 'Вы проиграли!')
-    else:
-        print(sum(number), 'Вы не проиграли')
 else:
     print(sum(number), 'Вы не проиграли')
+
+
