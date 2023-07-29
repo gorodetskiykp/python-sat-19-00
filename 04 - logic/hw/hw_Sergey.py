0fbd4f3ce64866d2161900a761e999aada04e243
sergej <sfomenko12@yandex.ru>
2023-07-29 19:14:29 +0300

    исправил ДЗ

--- 04 - logic/hw/hw_Sergey.py
+++ 04 - logic/hw/hw_Sergey.py
@@ -1,34 +1,49 @@
 from random import randint
 
 
-counter_card = 2
-check_card = randint(2, 11) + randint(2, 11)
-print("Счет:", check_card)
+COUNTER_CARD = 2
+card_1 = randint(2, 11)
+card_2 = randint(2, 11)
+list_cards = [card_1, card_2]
+check_card = list_cards[0] + list_cards[1]
+print("Ваши карты:", list_cards, "Счет:", check_card)
 
 
 if check_card == 21:
     print("ВЫ ВЫЙГРАЛИ!")
-    print("Счет:", check_card)
+    print("Ваши карты:", list_cards, "Счет:", check_card)
 else:
-    while check_card < 21:
-        choice = input("Берем карту или нет? Ваш выбор:").upper()
-        if choice == "ДА":
-            counter_card += 1
-            new_card = randint(2, 11)
-            if new_card == 11:
-                new_card = 1
-                check_card += new_card
-                print("Счет:", check_card)
+    choice = input("Берем карту или нет? Ваш выбор:").upper()
+    if choice == "ДА":
+        COUNTER_CARD += 1
+        new_card = randint(2, 11)
+        list_cards.append(new_card)
+        for nominal in range(len(list_cards) - 1):
+            if nominal == 11:
+                list_cards.remove(nominal)
+                list_cards.append(1)
                 if check_card == 21:
                     print("ВЫ ВЫЙГРАЛИ!")
-                    print("Счет:", check_card)
-                    break
+                    print("Ваши карты:", list_cards, "Счет:", check_card)
+                elif check_card < 21:
+                    print("ВЫ НЕ ПРОИГРАЛИ!")
+                    print("Ваши карты:", list_cards, "Счет:", check_card)
+                else:
+                    print("ВЫ ПРОИГРАЛИ!")
+                    print("Ваши карты:", list_cards, "Счет:", check_card)
+                break
             else:
                 check_card += new_card
-                print("Счет:", check_card)
-        else:
-            print("ВЫ НЕ ПРОИГРАЛИ")
-            print("Счет:", check_card)
-            break
-    if check_card > 21:
-        print("ВЫ ПРОИГРАЛИ")
+                if check_card == 21:
+                    print("ВЫ ВЫЙГРАЛИ!")
+                    print("Ваши карты:", list_cards, "Счет:", check_card)
+                elif check_card < 21:
+                    print("ВЫ НЕ ПРОИГРАЛИ!")
+                    print("Ваши карты:", list_cards, "Счет:", check_card)
+                else:
+                    print("ВЫ ПРОИГРАЛИ!")
+                    print("Ваши карты:", list_cards, "Счет:", check_card)
+                break
+    else:
+        print("ВЫ НЕ ПРОИГРАЛИ")
+        print("Счет:", check_card)
