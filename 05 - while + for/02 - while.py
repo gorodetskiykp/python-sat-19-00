20c184c7d0f3fffb9d923941997c1a0431f82e37
GorodetskiyKP <gorodetskiykp@gmail.com>
2023-07-29 21:18:40 +0300

    Поправил материалы к 5 занятию

---
+++ 05 - while + for/02 - while.py
@@ -0,0 +1,22 @@
+apples = 5
+
+while apples > 0:
+    print('i have', apples, 'apples')
+    print('i eat 1 apple')
+    apples -= 1
+
+# while apples:
+#     print('i have', apples, 'apples')
+#     print('i eat 1 apple')
+#     apples -= 1
+
+# while True:
+#     if apples <= 0:
+#         break
+#     print('i have', apples, 'apples')
+#     print('i eat 1 apple')
+#     apples -= 1
+
+
+print('i have no apples')
+
---
+++ 05 - while + for/06 - range.py
@@ -0,0 +1,16 @@
+print(list(range(0, 10)))
+
+for item in range(6, 10):
+    print('-', item)
+
+print()
+for item in range(5):
+    print('-', item)
+
+print()
+for item in range(5, 20, 3):
+    print('-', item)
+
+print()
+for item in range(50, 20, -3):
+    print('-', item)
