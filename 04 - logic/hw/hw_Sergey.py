46722ddc1c4e9f1269a153f26d1179d3ab251a3b
sergej <sfomenko12@yandex.ru>
2023-07-29 21:46:44 +0300

    исправил ДЗ(5): все)) в предыдущих версиях, постоянно что-то отрабатывало не так. Эта версия должна быть финальная. Протестировал во всех случаях)

--- 04 - logic/hw/hw_Sergey.py
+++ 04 - logic/hw/hw_Sergey.py
@@ -1,49 +1,42 @@
 from random import randint
 
-
 COUNTER_CARD = 2
 card1 = randint(2, 11)
 card2 = randint(2, 11)
 list_cards = [card1, card2]
-check_card = list_cards[0] + list_cards[1]
-print("Ваши карты:", list_cards, "Счет:", check_card)
+start_check = list_cards[0] + list_cards[1]
+print("Ваши карты:", list_cards, "Счет:", start_check)
 
-
-if check_card == 21:
+if start_check == 21:
     print("ВЫ ВЫЙГРАЛИ!")
-elif check_card > 21:
+elif start_check > 21:
     print("ВЫ ПРОИГРАЛИ!")
 else:
     choice = input("Берем карту или нет? Ваш выбор:").upper()
     if choice == "ДА":
-        COUNTER_CARD += 1
-        new_card = randint(2, 11)
-        list_cards.append(new_card)
-        for nominal in range(len(list_cards) - 1):  # цикл для перебора карт и проверки номинала
+        for nominal in range(len(list_cards)):
             if list_cards[nominal] == 11:
                 list_cards[nominal] = 1
-                if check_card == 21:
-                    print("ВЫ ВЫЙГРАЛИ!")
-                    print("Ваши карты:", list_cards, "Счет:", check_card)
-                elif check_card < 21:
-                    print("ВЫ НЕ ПРОИГРАЛИ!")
-                    print("Ваши карты:", list_cards, "Счет:", check_card)
-                else:
-                    print("ВЫ ПРОИГРАЛИ!")
-                    print("Ваши карты:", list_cards, "Счет:", check_card)
-                break
+                check_card = list_cards[0] + list_cards[1]
+        COUNTER_CARD += 1
+        new_card = randint(2, 11)
+        if new_card == 11:
+            new_card = 1
+            list_cards.append(1)
+            if check_card == 21:
+                print("ВЫ ВЫЙГРАЛИ!")
+                print("Ваши карты:", list_cards, "Счет:", check_card)
+            elif check_card < 21:
+                print("ВЫ НЕ ПРОИГРАЛИ!")
+                print("Ваши карты:", list_cards, "Счет:", check_card)
             else:
-                check_card += new_card
-                if check_card == 21:
-                    print("ВЫ ВЫЙГРАЛИ!")
-                    print("Ваши карты:", list_cards, "Счет:", check_card)
-                elif check_card < 21:
-                    print("ВЫ НЕ ПРОИГРАЛИ!")
-                    print("Ваши карты:", list_cards, "Счет:", check_card)
-                else:
-                    print("ВЫ ПРОИГРАЛИ!")
-                    print("Ваши карты:", list_cards, "Счет:", check_card)
-                break
+                print("ВЫ ПРОИГРАЛИ!")
+                print("Ваши карты:", list_cards, "Счет:", check_card)
+        else:
+            list_cards.append(new_card)
+            check_card = list_cards[0] + list_cards[1] + new_card
+            print("ВЫ НЕ ПРОИГРАЛИ!")
+            print("Ваши карты:", list_cards, "Счет:", check_card)
     else:
-        print("ВЫ НЕ ПРОИГРАЛИ")
-        print("Счет:", check_card)
+        print("ВЫ НЕ ПРОИГРАЛИ!")
+        print("Ваши карты:", list_cards, "Счет:", start_check)
