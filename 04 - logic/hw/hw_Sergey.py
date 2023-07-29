c18bd08385ca5672e9864f9041046181e7c46364
sergej <sfomenko12@yandex.ru>
2023-07-29 19:57:35 +0300

    исправил ДЗ(3)

--- 04 - logic/hw/hw_Sergey.py
+++ 04 - logic/hw/hw_Sergey.py
@@ -2,9 +2,9 @@ from random import randint
 
 
 COUNTER_CARD = 2
-card_1 = randint(2, 11)
-card_2 = randint(2, 11)
-list_cards = [card_1, card_2]
+card1 = randint(2, 11)
+card2 = randint(2, 11)
+list_cards = [card1, card2]
 check_card = list_cards[0] + list_cards[1]
 print("Ваши карты:", list_cards, "Счет:", check_card)
 
@@ -12,13 +12,15 @@ print("Ваши карты:", list_cards, "Счет:", check_card)
 if check_card == 21:
     print("ВЫ ВЫЙГРАЛИ!")
     print("Ваши карты:", list_cards, "Счет:", check_card)
+elif check_card > 21:
+    print("ВЫ ПРОИГРАЛИ!")
 else:
     choice = input("Берем карту или нет? Ваш выбор:").upper()
     if choice == "ДА":
         COUNTER_CARD += 1
         new_card = randint(2, 11)
         list_cards.append(new_card)
-        for nominal in range(len(list_cards) - 1):
+        for nominal in range(len(list_cards) - 1):  # цикл для перебора карт и проверки номинала
             if list_cards[nominal] == 11:
                 list_cards[nominal] = 1
                 if check_card == 21:
