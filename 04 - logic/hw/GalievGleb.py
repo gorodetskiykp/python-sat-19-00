9bd6b6c36f46092d6ff798d7e5860bcf9ca3a97d
GlebAQA <galievgleb99@gmail.com>
2023-08-05 19:16:02 +0300

    refactor

--- 04 - logic/hw/GalievGleb.py
+++ 04 - logic/hw/GalievGleb.py
@@ -1,36 +1,36 @@
 from random import randint
 
-number1 = randint(2, 11)
-number2 = randint(2, 11)
-number3 = randint(2, 11)
+def get_card():
+    return randint(2, 11)
 
-print(f"Ты получил {number1} и {number2}")
-choice = input("Нужна ли тебе карта?\nСкажи Yes или No: ")
+def play_game():
+    number1 = get_card()
+    number2 = get_card()
 
-if choice == "Yes":
-    print(f"Ты получил еще карту {number3}")
-    if number1 == 11:
-        number1 = 1
-    if number2 == 11:
-        number2 = 1
-    if number3 == 11:
-        number3 = 1
-    total_sum = sum([number1, number2, number3])
-    print(f"Теперь у тебя {total_sum}")
-    if total_sum < 21:
+    print(f"Ты получил {number1} и {number2}")
+
+    total_sum = number1 + number2
+
+    while total_sum <= 21:
+        choice = input("Нужна ли тебе карта?\nСкажи Yes или No: ")
+
+        if choice == "Yes":
+            new_card = get_card()
+            print(f"Ты получил еще карту {new_card}")
+            if new_card == 11 and total_sum + new_card > 21:
+                new_card = 1
+            total_sum += new_card
+            print(f"Теперь у тебя {total_sum}")
+        elif choice == "No":
+            break
+        else:
+            print("Ты ввел неверные входные данные, попробуй снова!")
+
+    if total_sum <= 21:
         print("Вы не проиграли")
-    elif total_sum == 21:
-        print("Вы выиграли")
     else:
         print("Вы проиграли")
-elif choice == "No":
-    total_sum = sum([number1, number2])
-    print(f"Твоя сумма карт: {total_sum}")
-    if total_sum < 21:
-        print("Вы не проиграли")
-    elif total_sum == 21:
-        print("Вы выиграли")
-    else:
-        print("Вы проиграли")
-else:
-    print("Ты ввел неверные входные данные, попробуй снова!")
+
+play_game()
+
+    
