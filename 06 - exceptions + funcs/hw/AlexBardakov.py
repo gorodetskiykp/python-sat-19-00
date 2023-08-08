d676949ef0fdc33d25e6be8834052308e4883fd7
KompukterMl <Battleover@yandex.ru>
2023-08-08 22:05:46 +0700

    Про упрощение списка понял, выполнил, в цикле все привел в соответствие, а вот как функцию упростить - понять не могу. По сути работает, но есть ощущение, что заклеил дыру в стене плакатом с Ларой Крофт.

--- 06 - exceptions + funcs/hw/AlexBardakov.py
+++ 06 - exceptions + funcs/hw/AlexBardakov.py
@@ -1,47 +1,30 @@
-number0 = ["****", "*  *", "*  *", "*  *", "****"]
-number1 = ["  * ", " ** ", "* * ", "  * ", "****"]
-number2 = [" ** ", "*  *", "   *", " *  ", "****"]
-number3 = [" ***", "   *", " ***", "   *", " ***"]
-number4 = ["   *", "  **", " * *", "****", "   *"]
-number5 = ["****", "*   ", "*** ", "   *", "*** "]
-number6 = ["****", "*   ", "****", "*  *", "****"]
-number7 = ["****", "  * ", " *  ", " *  ", " *  "]
-number8 = [" ** ", "*  *", " ** ", "*  *", " ** "]
-number9 = ["****", "*  *", "****", "   *", "****"]
+numbers = [["****", "*  *", "*  *", "*  *", "****"],
+           ["  * ", " ** ", "* * ", "  * ", "****"],
+           [" ** ", "*  *", "   *", " *  ", "****"],
+           [" ***", "   *", " ***", "   *", " ***"],
+           ["   *", "  **", " * *", "****", "   *"],
+           ["****", "*   ", "*** ", "   *", "*** "],
+           ["****", "*   ", "****", "*  *", "****"],
+           ["****", "  * ", " *  ", " *  ", " *  "],
+           [" ** ", "*  *", " ** ", "*  *", " ** "],
+           ["****", "*  *", "****", "   *", "****"]]
 
 
-def check_num(number):
-    if number == 0:
-        return number0
-    if number == 1:
-        return number1
-    elif number == 2:
-        return number2
-    elif number == 3:
-        return number3
-    elif number == 4:
-        return number4
-    elif number == 5:
-        return number5
-    elif number == 6:
-        return number6
-    elif number == 7:
-        return number7
-    elif number == 8:
-        return number8
-    elif number == 9:
-        return number9
-
-while True:
-    printer = []
-    try:
-        current_number = list(map(int, input("Введите число:")))
+def star_draw():
+    while True:
+        printer = []
+        try:
+            current_number = list(map(int, input("Введите число:")))
+        except ValueError:
+            print("Обязательно нужно ввести число")
+            continue
         for process in range(5):
-            for i in range(len(current_number)):
-                one = check_num(current_number[i])
-                printer.append(one[process])
+            for digit in current_number:
+                save_num = numbers[current_number[digit]]
+                printer.append(save_num[process])
             print(*printer)
             printer = []
         break
-    except ValueError as error1:
-        print("Обязательно нужно ввести число")
+
+
+star_draw()
