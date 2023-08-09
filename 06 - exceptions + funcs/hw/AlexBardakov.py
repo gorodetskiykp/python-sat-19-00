febda01450604fee020e339f81e3f434c654ef39
KompukterMl <Battleover@yandex.ru>
2023-08-10 00:29:06 +0700

    Сделал итоговое исполнение. Постарался учесть все правки, но не смог сделать, чтобы по вызову функции делалась вся программа, потому что при включении input функцию, при формировании строки из звездочек, input зацикливался в бесконечность.

--- 06 - exceptions + funcs/hw/AlexBardakov.py
+++ 06 - exceptions + funcs/hw/AlexBardakov.py
@@ -10,21 +10,28 @@ numbers = [["****", "*  *", "*  *", "*  *", "****"],
            ["****", "*  *", "****", "   *", "****"]]
 
 
-def star_draw():
-    while True:
-        printer = []
-        try:
-            current_number = list(map(int, input("Введите число:")))
-        except ValueError:
-            print("Обязательно нужно ввести число")
-            continue
-        for process in range(5):
-            for digit in current_number:
-                save_num = numbers[current_number[digit]]
-                printer.append(save_num[process])
-            print(*printer)
-            printer = []
-        break
+def star_line():  # создаем единый список символов в строку
+    intermediate = []
+    for process in range(5):
+        for digit in range(len(current_number)):
+            save_num = numbers[current_number[digit]]
+            intermediate.append(save_num[process])
+    return intermediate
 
 
-star_draw()
+def star_list():  # делим список по длине введеного числа
+    draw_list = (star_line()[i:i + 5] for i in range(0, len(star_line()), 5))
+    return list(draw_list)
+
+
+current_number = []
+while True:
+    try:
+        current_number = list(map(int, input("Введите число:")))
+    except ValueError:
+        print("Обязательно нужно ввести число")
+        continue
+    break
+for string in range(5):
+    print(*star_list()[string])
+
