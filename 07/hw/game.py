0a31240686373613cff1d0b5a9d114b9c8f04bb6
Togepi <khlyzova.diana@gmail.com>
2023-08-19 03:04:05 +0700

    Сделала view_cards

--- 07/hw/game.py
+++ 07/hw/game.py
@@ -1,5 +1,6 @@
 from players import get_players
 from cards import get_cards_for_players
+from random import randint
 
 def view_cards(players_cards):
     """
@@ -9,6 +10,24 @@ def view_cards(players_cards):
     ]
     Вывести на экран красиво
     """
+    for player in range(len(players_cards)):
+        for cards in range(6):
+            card = players_cards[player][1][cards]
+            if 'H' in card:
+                card = card.replace('H', '♥')
+            elif 'D' in card:
+                card = card.replace('D', '♦')
+            elif 'C' in card:
+                card = card.replace('C', '♣')
+            elif 'S' in card:
+                card = card.replace('S', '♠')
+            players_cards[player][1][cards] = card
+        print('Игрок', player + 1, '-', players_cards[player][0],
+              '\nКарты в руке:', *players_cards[player][1], '\n')
+    egg = randint(1, 100)  # пасхалка
+    if egg > 90:
+        print('Все игроки - пусечки!')
+
 
 def game():
     players_count = int(input("Укажите количество игроков: "))
