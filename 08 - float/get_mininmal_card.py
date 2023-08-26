2c3abb062c451df5ec31ef6fa36956a05637f31f
sfomenk0 <sfomenko12@yandex.ru>
2023-08-26 13:59:26 +0300

    исправил

--- 08 - float/get_mininmal_card.py
+++ 08 - float/get_mininmal_card.py
@@ -1,14 +1,8 @@
-
-hand = ['1K', '3B', '2H', '4C', '5K', '6K']
-tramp_mark = 'K'
-new_hand = []
-
-def get_min_card(hand):
-    for i in hand:
-        if tramp_mark in i:
-            continue
-        else:
-            new_hand.append(i)
+def get_min_card(hand: list, tramp_mark: str):
+    new_hand = []
+    for card in hand:
+        if tramp_mark not in card:
+            new_hand.append(card)
     return min(new_hand)
-print(get_min_card(hand))
+print(get_min_card(hand, tramp_mark))
 
