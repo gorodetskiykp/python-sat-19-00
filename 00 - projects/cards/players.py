a4701d6ec693cd238e03c8b5128c39ae67909d97
Togepi <khlyzova.diana@gmail.com>
2023-08-26 15:58:45 +0700

    Сделала проверку имени игрока >= 3 символа

--- 00 - projects/cards/players.py
+++ 00 - projects/cards/players.py
@@ -6,13 +6,16 @@ def get_players(players_count: int) -> list:
     players_names = []
     while len(players) < players_count:
         name = input('Введите имя игрока: ').strip()
-        if name in black_list:
-            print('Игрок в чёрном списке')
-        elif name in players_names:
-            print('Игрок уже добавлен')
+        if len(name) >= 3:
+            if name in black_list:
+                print('Игрок в чёрном списке')
+            elif name in players_names:
+                print('Игрок уже добавлен')
+            else:
+                players.append([name, []])
+                players_names.append(name)
         else:
-            players.append([name, []])
-            players_names.append(name)
+            print('Имя должно содержать минимум 3 символа. Попробуйте ещё раз')
     return players
 
 
