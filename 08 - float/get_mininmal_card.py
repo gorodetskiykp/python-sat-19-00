750a7693a1a899a32c5fac4c7fdca4640037a610
sfomenk0 <sfomenko12@yandex.ru>
2023-08-26 14:00:36 +0300

    исправил(1)

--- 08 - float/get_mininmal_card.py
+++ 08 - float/get_mininmal_card.py
@@ -4,5 +4,5 @@ def get_min_card(hand: list, tramp_mark: str):
         if tramp_mark not in card:
             new_hand.append(card)
     return min(new_hand)
-print(get_min_card(hand, tramp_mark))
+
 
