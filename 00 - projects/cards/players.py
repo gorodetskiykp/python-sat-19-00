a340dd366f0aa759a5e0103a914a5aec78f9afc3
KompukterMl <Battleover@yandex.ru>
2023-09-08 01:59:35 +0700

    Сделал defence, помимо этого изменил вывод функции с card на hand, и дополнительно добавил функцию movement, реализуемую через move. Добавил тесты для defence и movement

--- 00 - projects/cards/players.py
+++ 00 - projects/cards/players.py
@@ -1,6 +1,6 @@
 from random import randint
 
-from config import black_list
+from config import black_list, cards
 from cards import get_minimal_card
 
 
@@ -61,4 +61,82 @@ def move(hand: list, card: str):
         Строчное значение карты
     """
     hand.remove(card)
-    return card
+    return hand
+
+
+def movement(hand: list):
+    """Убрать карту из карт игрока.
+
+    Аргументы:
+        hand - список карт у игрока
+        card - строчное значение карты, которую нужно выложить на стол
+    Возвращаемое значение:
+        Строчное значение карты
+    """
+    table = []
+    row = []
+    print('У вас на руке:', *hand)
+    card_num = int(input('Какой картой вы хотите сходить?(1,2,..,6,..)')) - 1
+    card = hand[card_num]
+    hand = move(hand, card)
+    table.append(list(card))
+    for cards in hand:
+        if card[:-1] in cards:
+            row.append(cards)
+    print('Вы так же можете выложить', *row)
+    if input('Будем выкладывать?(да/нет)').strip().lower() == 'да':
+        for cards in row:
+            table.append(list(cards))
+            hand = move(hand, cards)
+    return table
+
+
+def defence(hand: list, table: list, trump_mark: str):
+    take_to_hands = False  # если не отбили - меняем на True
+    for tab_cards in table:
+        skip = ''  # для игнорирования карты, которой только что отбились
+        for card in tab_cards:
+            if skip == card:
+                continue
+            stop_index = 1  # параметр сработает только если не сможем отбиться
+            bit_bat = []  # список карт, которыми сможем отбить карту со стола
+            for cardstr in hand:
+                card_value = card[:-1]  # убираем масть у карты для сравнения
+                cardstr_value = cardstr[:-1]
+                # добавляем сравнение карт по старшенству.
+                if (card[-1] in cardstr and
+                        cards.index(cardstr_value) > cards.index(card_value)):
+                    bit_bat.append(cardstr)
+                # избегаем сравнения козырей между собой, они отпадут в if
+                elif trump_mark in cardstr and trump_mark != cardstr[-1]:
+                    bit_bat.append(cardstr)
+            print('Ваша рука:', *hand)
+            print('Карты на столе:', *table)
+            if bit_bat:
+                print('Вы можете отбить', card, 'c помощью',
+                      ', '.join(map(str, bit_bat)),
+                      'Чем будем отбивать?(1,2,3,...)')
+                while True:
+                    try:
+                        num = int(input('Введите номер карты'))
+                        break
+                    except:
+                        print('Введите существующий порядковый номер карты')
+                        continue
+                bit = bit_bat[num - 1]
+                print('Отбиваем', bit)
+                hand.remove(bit)
+                tab_cards.append(bit)
+                skip = bit  # игнорируем карту, которой отбились в сл.итерации
+                stop_index = 0
+            else:
+                print('Отбить не получится, забираем карты на руку')
+            if stop_index == 1:
+                take_to_hands = True
+                break
+    print('Карты на столе:', *table)
+    if take_to_hands:
+        for tab_cards in table:
+            for card in tab_cards:
+                hand.append(card)
+    return hand
