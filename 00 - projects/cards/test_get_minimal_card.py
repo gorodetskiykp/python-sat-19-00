24955267f9c274e33b3bffbc28c3f9f92112c96c
Togepi <khlyzova.diana@gmail.com>
2023-09-09 03:48:52 +0700

    Добавила тесты для get_minimal_card

--- 00 - projects/cards/test_get_minimal_card.py
+++ 00 - projects/cards/test_get_minimal_card.py
@@ -1,4 +1,5 @@
 from cards import get_minimal_card, sorted_cards
+from config import cards, marks
 from test_config import (
     minimal_card_in_all_stack,
     stack,
@@ -8,3 +9,7 @@ from test_config import (
 assert get_minimal_card(stack) == minimal_card_in_all_stack
 assert get_minimal_card([]) == None
 assert get_minimal_card(stack, trump) == minimal_card_in_all_stack
+assert isinstance(get_minimal_card(stack, trump), str)
+assert 2 <= len(get_minimal_card(stack, trump)) <= 3
+assert get_minimal_card(stack, trump)[:1] in cards
+assert get_minimal_card(stack, trump)[-1] in marks
