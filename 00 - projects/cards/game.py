ef99bed9ecd2a3412bbca7388375dd040a067415
Andrei-Kanaev <124676820+Andrei-Kanaev@users.noreply.github.com>
2023-10-12 22:39:31 +0300

    rewrite get_cards_for_players (#87)
    * rewrite get_cards_for_players
    
    * Поправил выбор добор карт
    
    ---------
    
    Co-authored-by: GorodetskiyKP <gorodetskiykp@gmail.com>

--- 00 - projects/cards/game.py
+++ 00 - projects/cards/game.py
@@ -11,9 +11,10 @@ from cards import (
     get_trump_card,
     move,
     sorted_cards,
+    get_next_player
 )
 
-from players import first_move, get_next_player, get_players
+from players import first_move, get_players
 
 
 def view_cards(players) -> None:
@@ -28,8 +29,8 @@ def view_cards(players) -> None:
     Returns:
         None
     """
-    for player, player_cards in players:
-        print(player, '-', *sorted_cards(player_cards))
+    for player_name, player_cards in players:
+        print(player_name, '-', *sorted_cards(player_cards))
 
 
 def game():
@@ -42,39 +43,47 @@ def game():
     #     print('Может быть только от 2 до 4 игроков')
     #
     # players = get_players(players_count)
-    players_count = 2
+    players_count = 5
     players = [
         ["Player1", []],
         ["Player2", []],
+        ["Player3", []],
+        ["Player4", []],
+        ["Player5", []],
     ]
     stack = get_stack()
     players = get_cards_for_players(stack, players)
     trump = get_trump_card(stack, players)
     view_cards(players)
     print('Козырь: {}'.format(trump))
+    # while True:
     next_move = first_move(players, trump)
-    print('Игру начинает {}'.format(players[next_move][0]))
-
-    cards_on_desk = choose_attacking_cards(players[next_move][-1], trump, stack)
-    print('Карты на столе: {}'.format(', '.join(cards_on_desk)))
+    print('Атакует {}'.format(players[next_move][0]))
     defender_index = get_next_player(next_move, players_count)
     print('Защищается {}'.format(players[defender_index][0]))
-    cards_to_defend = defence(players[defender_index][-1], cards_on_desk, trump)
-    # если атакующих карт больше, чем карт на руке
-    # и смог потратить все свои карты
-    # вернуть какой-то признак, чтобы атакующий забрал свои небитые карты
-    if cards_to_defend is not None:
-        print('Игрок {} отбивает: {}'.format(players[defender_index][0],
-                                             cards_to_defend))
+    cards_on_desk = {}
+    while True:
         cards_on_desk = choose_attacking_cards(players[next_move][-1], trump,
-                                               stack, cards_to_defend)
-        print("Карты  после второй атаки: {}".format(cards_on_desk))
-    else:
-        print('Игрок {} не может отбить карту'
-              .format(players[defender_index][0]))
+                                               stack, cards_on_desk)
+        if cards_on_desk is None:
+            print('Переходим к следующему игроку')
+            break
+        print('Карты на столе: {}'.format(', '.join(cards_on_desk)))  # пока выводит только ключи, нужно добавить и значения
+        cards_on_desk = defence(players[defender_index][-1], cards_on_desk, trump)
+        # если атакующих карт больше, чем карт на руке
+        # и смог потратить все свои карты
+        # вернуть какой-то признак, чтобы атакующий забрал свои небитые карты
+        if cards_on_desk is None:
+            print('Игрок {} не может отбить карты'
+                  .format(players[defender_index][0]))
+            break
+        else:
+            print('Игрок {} отбивает: {}'.format(players[defender_index][0],
+                                                 cards_on_desk))
+        view_cards(players)
 
-    view_cards(players)
-    players = get_cards_for_players(stack, players)     
+
+    players = get_cards_for_players(stack, players, next_move)
     view_cards(players)
 
 
