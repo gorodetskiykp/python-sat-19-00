a0e1162b4b6aa88eaf04b666cca882a06a0d9a24
YakoDmi <136105744+Pipidastter@users.noreply.github.com>
2023-10-30 20:01:50 +0300

    Исправил assert для функции allocate_cards

--- 00 - projects/cards/test_allocate_cards.py
+++ 00 - projects/cards/test_allocate_cards.py
@@ -1,5 +1,7 @@
 from cards import allocate_cards
 from test_config import stack
 stack_2 = ['A♥', '5♣', '6♠', 'K♠', '4♥', '7♦',  'Q♠', '10♠']
-assert allocate_cards(stack)
-assert allocate_cards(stack_2)
+
+assert allocate_cards(stack) == {'♥': ['5♥', '8♥', 'A♥'], '♦': ['7♦'], '♣': [], '♠': ['6♠', '7♠', '10♠', 'Q♠']}
+assert allocate_cards(stack_2) == {'♥': ['4♥', 'A♥'], '♦': ['7♦'], '♣': ['5♣'], '♠': ['6♠', '10♠', 'Q♠', 'K♠']}
+
